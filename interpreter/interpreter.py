02205aa3630d716f0d97e1ec5ff1a961ea19cbb0
mugiwaranopony <mariam.lashkhia1@gmail.com>
2026-02-27 18:07:21 +0000

    does this work?

---
+++ interpreter/interpreter.py
@@ -0,0 +1,15 @@
+expression = input("expression: ").strip()
+x, y, z = expression.split()
+x = int(x)
+z = int(z)
+
+if y == "+":
+    result = x + z
+elif y == "-":
+    result = x - z
+elif y == "*":
+    result = x * z
+elif y == "/":
+    result = x / z
+
+print (f"{result:.1f}")
