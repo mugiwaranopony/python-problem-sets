6b2d75b2795fdf05c9ad183be2058e2755c2d8c2
mugiwaranopony <mariam.lashkhia1@gmail.com>
2026-03-02 14:28:26 +0000

    final?

--- bank/bank.py
+++ bank/bank.py
@@ -2,9 +2,9 @@ greeting = input("Greeting: ")
 
 greeting = greeting.strip().lower()
 
-if greeting.startswith("hello") or greeting.startswith("Hello"):
+if greeting.startswith("hello"):
     print("You get 0$")
-elif greeting.startswith("h") or greeting.startswith("H"):
+elif greeting.startswith("h"):
     print("You get 20$")
 else:
     print("You get 100$")
