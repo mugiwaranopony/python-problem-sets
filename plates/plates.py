638e4eebf7e937f10832b12f22571e5095481f42
mugiwaranopony <mariam.lashkhia1@gmail.com>
2026-08-11 08:36:50 +0000

    plates

---
+++ plates/plates.py
@@ -0,0 +1,35 @@
+def main():
+    plate = input("Plate: ")
+
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+
+    if not s[:2].isalpha():
+        return False
+
+    if not s.isalnum():
+        return False
+
+    number_started = False
+
+    for character in s:
+        if character.isdigit():
+            if not number_started:
+                if character == "0":
+                    return False
+                number_started = True
+        elif number_started:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    main()
