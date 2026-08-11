1c3f18fbf3d40fc37017d595dc68ab74096476a3
mugiwaranopony <mariam.lashkhia1@gmail.com>
2026-08-11 10:31:27 +0000

    figlet

---
+++ figlet/figlet.py
@@ -0,0 +1,30 @@
+import random
+import sys
+
+from pyfiglet import Figlet
+
+
+def main():
+    figlet = Figlet()
+    fonts = figlet.getFonts()
+
+    if len(sys.argv) == 1:
+        font = random.choice(fonts)
+    elif (
+        len(sys.argv) == 3
+        and sys.argv[1] in ["-f", "--font"]
+        and sys.argv[2] in fonts
+    ):
+        font = sys.argv[2]
+    else:
+        sys.exit("Invalid usage")
+
+    figlet.setFont(font=font)
+    text = input("Input: ")
+
+    print("Output:")
+    print(figlet.renderText(text))
+
+
+if __name__ == "__main__":
+    main()
